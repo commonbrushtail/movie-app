60ea637bf7875c5861c08d7e9f82e9c23655ba1a
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-03 21:57:59 +0700

    Updates

---
+++ precache-manifest.a874d8109640d19b10b6f13e9813e46d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed3f8c7f4c5d859e354155a87c594af",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7d5fb9355c8b6cfe7b32",
+    "url": "/movie-app/static/css/main.30dda5f3.chunk.css"
+  },
+  {
+    "revision": "e3792f61574915c594ac",
+    "url": "/movie-app/static/js/2.4ca71b7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.4ca71b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5fb9355c8b6cfe7b32",
+    "url": "/movie-app/static/js/main.39aa8a75.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
