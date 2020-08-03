90a519917b69d4db4145092c3bd0f77df9397c25
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-03 22:16:26 +0700

    dd

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,11 +7,11 @@ export default function Footer(props) {
         <form action =""  onChange={props.handleChange} onSubmit={props.handleSubmit}className ="searchBar">
             <div style={{'display':'flex'}}>
                 <input className ="inputBox"type="text" name="search" />
-                <button className ="submit" type="submit"><img alt="serachLogo" src="search.png"/></button>
+                <button className ="submit" type="submit"><img alt="serachLogo" src="/search.png"/></button>
             </div>
-                <div style={{'display':'flex','flexDirection':'column','alignItems':'center','marginTop':'9px'}}>
+                <div style={{'display':'flex','flexDirection':'column','alignItems':'center','marginTop':'10px'}}>
             
-                    <img alt="logo" style ={{'width':'27px'}}src="Tmdb-312x276-logo.png" />
+                    <img alt="logo" style ={{'width':'27px'}}src="/Tmdb-312x276-logo.png" />
                     <p style={{'fontSize':'5px'}}>Powered by</p>
                 </div>
                 
