64503cbe2eaf77a0c859c0d6f815ce6e14f57084
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-03 22:05:35 +0700

    d

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
                 <input className ="inputBox"type="text" name="search" />
                 <button className ="submit" type="submit"><img alt="serachLogo" src="search.png"/></button>
             </div>
-                <div style={{'display':'flex','flexDirection':'column','alignItems':'center','marginTop':'10px'}}>
+                <div style={{'display':'flex','flexDirection':'column','alignItems':'center','marginTop':'9px'}}>
             
                     <img alt="logo" style ={{'width':'27px'}}src="Tmdb-312x276-logo.png" />
                     <p style={{'fontSize':'5px'}}>Powered by</p>
